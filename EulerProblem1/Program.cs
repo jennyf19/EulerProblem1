1577b68dd0cc343ec13338d4c9a61c4afbfdf0f1
Jennyf19 <ferriesj@gmail.com>
2016-07-27 16:23:21 -0700

    add multiple

--- EulerProblem1/Program.cs
+++ EulerProblem1/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EulerProblem1
+namespace EulerProblems
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                int x = i % 3;
+                int y = i % 5;
+                if (x != 0 && y != 0) continue;
+                Console.Write("{0}\t", i);
+                sum = sum + i;
+            }
+            Console.WriteLine("The Sum of all the multiples of 3 or 5 below 1000 is " + sum);
+            Console.Read();
         }
     }
 }
